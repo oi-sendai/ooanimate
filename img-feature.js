e4a8fb7bb242f9d2eaa73a981bb222a158130b53
oi-sendai <binarygeometry@gmail.com>
2015-05-26 13:10:44 +0100

    added process handler

--- img-feature.js
+++ img-feature.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
 
+
+  $('#contact').validate({
+
+    submitHandler: function(form) {
+      $(form).ajaxSubmit({
+        url: 'process.php',
+        success: function() {
+          $('#contact').hide();
+          $('#contact-form').append("<p class='thanks'>Thanks! Your request has been sent.</p>")
+        }
+      })
+    }
+})
+
+
 	// function MyClass(arg) {
 
 	// 	this.arg = arg;
